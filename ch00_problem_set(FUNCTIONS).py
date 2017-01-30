6104d7b450460c5359a4bbc720a55a5d0db0a349
nsachs <nsachs@fwparker.org>
2017-01-30 13:57:07 -0600

    final

--- ch00_problem_set(FUNCTIONS).py
+++ ch00_problem_set(FUNCTIONS).py
@@ -4,6 +4,12 @@
 # Make a function which asks the user to enter a string, then prints the length of that string.
 # You will need to use the input() function.
 # Make a call to that function
+def length(string):
+    print(len(string))
+
+length(input("Enter some words: "))
+
+print()
 
 # PROBLEM 2 (Pythagorean theorem - 4pts)
 # The Pythagorean theorem states that of a right triangle, the square of the
@@ -12,20 +18,76 @@
 # Write a program that asks the user for the lengths of the two sides that meet at a right angle.
 # Then calculate the length of the third side, and display it in a nicely formatted way.
 # You may ignore the fact that the user can enter negative or zero lengths for the sides.
+def triangle(a, b):
+    c = (int(a) ** 2 + int(b) ** 2) ** 0.5
+    print("The length of the third side is: ", c)
+
+triangle(int(input("Enter length of side 1: ")), input("Enter length of side 2: "))
+
+print()
 
 # PROBLEM 3 (Biggest, smallest, average - 4pts)
 # Make a function to ask the user to enter three numbers.
 # Then print the largest, the smallest, and their average, rounded to 2 decimals.
 # Display the answers in a "nicely" formatted way.
 # Make a call to your function.
+def sequence(first, second, third):
+
+    if first >= second and first >= third:
+        print(first, "is the largest number.")
+    elif second >= first and second >= third:
+        print(second, "is the largest number.")
+    else:
+        print(third, "is the largest number.")
+
+    if first <= second and first <= third:
+        print(second, "is the smallest number.")
+    elif second <= first and second <= third:
+        print(second, "is the smallest number.")
+    else:
+        print(third, "is the smallest number.")
+
+    average = round((first + second + third)/3, 2)
+    print("The average of the numbers is", average)
+
+sequence(int(input("Enter a number: ")), int(input("Enter a number: ")), int(input("Enter a number: ")))
+
+print()
 
 # PROBLEM 4 (e to the... - 3pts)
 # Calculate the value of e (from the math library) to the power of -1, 0, 1, 2, and 3.
 # display the results, with 5 decimals, in a nicely formatted manner.
+import math
 
+def find(x):
+    solution = math.e ** x
+    print(round(solution,5))
+
+find(-1)
+find(0)
+find(1)
+find(2)
+find(3)
+print()
 # PROBLEM 5 (Random int - 3pts)
 # Generate a random integer between 1 and 10 (1 and 10 both included),
 # but only use the random() function (randrange is not allowed here)
+import random
+
+def integer(k):
+    k *= 10
+    if k < 1:
+        k +=1
+    print(round(k))
+
+integer(random.random())
+
+print()
+
 
 # PROBLEM 6 (add me, multiply me - 3pts)
 # Make a function which takes in two integers and RETURNS their sum AND their product.
+def calculator(first_number, second_number):
+    return first_number + second_number, first_number * second_number
+
+print(calculator(int(input("Enter a number: ")),int(input("Enter another number: "))))
--- ch00_problem_set(LOOPS).py
+++ ch00_problem_set(LOOPS).py
@@ -6,6 +6,15 @@
 # I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
+a = 1
+b = 1
+
+while a < 1000 and b < 1000:
+    print(a)
+    print(b)
+    a += b
+    b += a
+print()
 
 
 # PROBLEM 2 (Number Guessing Game - 6pts)
@@ -16,6 +25,22 @@
 # and “You guessed it!” if the number it took is equal to the number
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
+import random
+
+computer = random.randrange(1,1001)
+print(computer)
+
+done = False
+while not done:
+    guess = int(input("Enter a number between 1 and 1000: "))
+    if guess > computer:
+        print("Lower")
+    elif guess < computer:
+        print("Higher")
+    elif guess == computer:
+        done = True
+print("You guessed it!")
+print()
 
 # PROBLEM 3 (Dice Hi-Low - 6pts)
 # You roll five six-sided dice, one by one.
@@ -24,6 +49,21 @@
 # For example, the sequence “1, 1, 4, 4, 6” is a success,
 # but “1, 1, 4, 3, 6” is not. Determine the
 # probability of success using a simulation of a large number of trials.
+c = [0]
+percent_success = 0
+for i in range(10000):
+    x = [0]
+    for i in range(1, 6):
+        y = random.randrange(1, 7)/ i
+        x.append(y * i)
+        if y * i < x[i - 1]:
+            percent_success += 1
+    c.append(x)
+
+percent_success /= 50000
+print("The likelihood that the value previously rolled is lower every time is: ", round(percent_success * 100, 2), "%.")
+
+print()
 
 # PROBLEM 4 (Number Puzzler - 6pts)
 # A, B, C, and D are all different digits.
@@ -31,4 +71,10 @@
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
 # Use a quadruple-nested loop to solve.
+for a in range(1, 10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(1, 10):
+                if int(str(d) + str(c) + str(b) + str(a)) == 4 * int(str(a) + str(b) + str(c) + str(d)):
+                    print(a, b, c, d)
 
